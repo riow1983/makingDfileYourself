be17b67b07461251d63ebd61b2c5c1623a46870f
Horiuchi <rhoriuchi@jp.imshealth.com>
2019-03-12 16:10:30 +0900

    initial commit

---
+++ sql/CREATE_dpc_order.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE dpc_order
+(患者番号 INTEGER,
+患者氏名 VARCHAR(255),
+DPCコード VARCHAR(255),
+診断群分類名称 VARCHAR(255),
+入院日（転入日） DATE,
+退院日（転出日） DATE,
+入院期間Ⅰ INTEGER,
+入院期間Ⅱ INTEGER,
+入院期間Ⅲ INTEGER,
+DPC対象外区分 BOOLEAN,
+id SERIAL PRIMARY KEY);
